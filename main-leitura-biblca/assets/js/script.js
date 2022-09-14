73164a0ea89e6c74ae277d0dbbe6c43b6abf6cb0
Gilberto Mascena <gilbertomascena@gmail.com>
2022-09-14 20:38:26 -0300

    Adicionando funções.

--- main-leitura-biblca/assets/js/script.js
+++ main-leitura-biblca/assets/js/script.js
@@ -1,26 +1,27 @@
 /* Método JS para abir links e nova aba. */
 
-const urlLink = "https://www.linkedin.com/in/gilberto-mascena-96393723a";
+const urlLinkedin = "https://www.linkedin.com/in/gilberto-mascena-96393723a";
 const linkedin = document.querySelector(".social__linkedin");
 
-const urlGit = "https://github.com/Gilberto-Mascena"
-const github = document.querySelector(".socila__github");
+const urlGitHub = "https://github.com/Gilberto-Mascena"
+const gitHub = document.querySelector(".social__github");
 
 
-function openInNewTab(urlLink) {
-    const novaAbaLink = window.open(urlLink, "_blank");
+function openInNewTab(urlLinkedin) {
+    const novaAbaLink = window.open(urlLinkedin, "_blank");
     novaAba.focus();
 }
 
 linkedin.addEventListener("click", () => {
-    openInNewTab(urlLink);
+    openInNewTab(urlLinkedin);
 })
 
 
-function openInNewTab(urlGit) {
-    const novaAbaGit = window.open(urlGit, "_blank");
+function openInNewTab(urlGitHub) {
+    const novaAbaGit = window.open(urlGitHub, "_blank");
+    novaAba.focus();
 }
 
-github.addEventListener("click", () => {
-    openInNewTab(urlGit);
+gitHub.addEventListener("click", () => {
+    openInNewTab(urlGitHub);
 })
